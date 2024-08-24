6011fe103f866bb2c77f9b54c01994dad31db00d
Filipkin Denis <42109109+egoson@users.noreply.github.com>
2024-08-24 11:36:09 +0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@
  *
  *
  * For the service to work, you must connect the S3 storage to Default Uploader
- * @see https://docs.defaultuploader.com/#/?id=%d0%9a%d0%b0%d0%ba-%d0%bf%d0%be%d0%b4%d0%ba%d0%bb%d1%8e%d1%87%d0%b8%d1%82%d1%8c-%d0%bf%d1%80%d0%be%d0%b2%d0%b0%d0%b9%d0%b4%d0%b5%d1%80%d0%b0-%d0%be%d0%b1%d1%8a%d0%b5%d0%ba%d1%82%d0%bd%d0%be%d0%b3%d0%be-%d1%85%d1%80%d0%b0%d0%bd%d0%b8%d0%bb%d0%b8%d1%89%d0%b0 Default Uploader setup
+ * @see https://docs.defaultuploader.com/en/start/s3-storage Default Uploader setup
  */
 export default function (clientToken: string) {
     return ({ src, width, quality }: { src: string, width: number | undefined, quality?: number | undefined }) => {
